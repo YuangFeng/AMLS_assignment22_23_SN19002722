949ac42ce94195e77f1bec21b3da2080316ec691
zceefen <101616750+YuangFeng@users.noreply.github.com>
2022-11-27 11:27:54 +0000

    Add more comments

--- B1/model_b1.py
+++ B1/model_b1.py
@@ -3,14 +3,36 @@ from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 from sklearn.model_selection import cross_val_score
 class Model_B1:
+    """
+    Random Forest is used for task B1
+    """
     def __init__(self) -> None:
         self.model = RandomForestClassifier(n_estimators=50, n_jobs=-1)
     def train(self, x, y):
+        """
+        Train the model
+        Input parameters:
+            x: pre-processed images in training set
+            y: labels of images in training set
+        Returns:
+            None
+        """
         # scores = cross_val_score(self.model, x, y, cv=10)
         # print('K-fold scores:', scores)
         self.model = self.model.fit(x,y)
     
     def test(self, x, y):
+        """
+        Test the trained model
+        Input parameters:
+            x: images in testing set
+            y: labels of images in testing set
+        Returns:
+            acc: accuracy of model 
+            p_class: precision of model
+            r_class: Recall of model
+            f_class: F1 score of model
+        """
         pred = self.model.predict(x)
         acc = accuracy_score(y, pred)
         p_class, r_class, f_class, support_micro = precision_recall_fscore_support(y, pred)
