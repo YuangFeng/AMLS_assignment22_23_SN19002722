f419aca14c75b897729d6fcb906e64664182f7c3
zceefen <101616750+YuangFeng@users.noreply.github.com>
2022-11-27 11:17:59 +0000

    Add more commets

--- A1/model_a1.py
+++ A1/model_a1.py
@@ -2,10 +2,13 @@
 import torch.nn as nn
 
 class ModelA1(nn.Module):
+    """
+    CNN is used for task A1, with multiple layers
+    """
     def __init__(self, num_classes = 1) -> None:
         super().__init__()
         self.num_classes = num_classes
-        #CNN model
+        #Layers of CNN model
         self.layers = nn.Sequential(
             nn.Conv2d(3,64,3,padding=1),
             nn.BatchNorm2d(64),
@@ -27,6 +30,13 @@ class ModelA1(nn.Module):
             nn.Linear(128, self.num_classes)
         )
     def forward(self, x):
+        """
+        Forward images 
+        Input:
+            Preprocessed image
+        Return:
+            Output of model
+        """
         x = self.layers(x)
         x = x.flatten(1)
         x = self.linear(x)
